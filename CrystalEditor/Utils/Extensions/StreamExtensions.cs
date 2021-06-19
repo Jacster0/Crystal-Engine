5b0de33cf5e7a76466634552cd443e3caab52564
Jacster0 <jacob.ogren2@gmail.com>
2021-06-19 23:23:18 +0200

    Read and write byte buffers into structs

---
+++ CrystalEditor/Utils/Extensions/StreamExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.IO;
+
+namespace CrystalEditor.Utils.Extensions
+{
+    public static class StreamExtensions
+    {
+        private const int MAX_STACKALLOC_SIZE = 512;
+
+        public static T ReadStruct<T>(this Stream stream) where T : struct
+        {
+            var length = Marshal.SizeOf<T>();
+            Span<byte> buffer = (length <= MAX_STACKALLOC_SIZE) ? stackalloc byte[length] : new byte[length];
+
+            stream.Read(buffer);
+            return MemoryMarshal.Read<T>(buffer);
+        }
+
+        public static void WriteStruct<T>(this Stream stream, ref T structure) where T : struct
+        {
+            var length = Marshal.SizeOf<T>();
+            Span<byte> buffer = (length <= MAX_STACKALLOC_SIZE) ? stackalloc byte[length] : new byte[length];
+
+            MemoryMarshal.Write(buffer, ref structure);
+            stream.Write(buffer);
+        }
+    }
+}
