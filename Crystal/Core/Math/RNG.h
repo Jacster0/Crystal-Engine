a44d6d2233151f55e1f7a547878550a42ed536af
Jacster0 <jacob.ogren2@gmail.com>
2021-07-03 16:24:53 +0200

    Added a RNG

---
+++ Crystal/Core/Math/RNG.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <random>
+#include <concepts>
+#include <algorithm>
+
+namespace Crystal::Math {
+	class Rng {
+	public:
+		template<typename T>
+		requires std::integral<T>
+		[[nodiscard]] static constexpr inline auto Random(T low, T high) noexcept {
+			std::default_random_engine engine{ std::random_device{}() };
+
+			//This should never happen but we will support it anyway
+			if (high < low) [[unlikely]] {
+				std::swap(low,high);
+			}
+
+			return std::uniform_int_distribution<T>{ low, high }(engine);
+		}
+
+		template<typename T>
+		requires std::floating_point<T>
+		[[nodiscard]] static constexpr inline auto Random(T low, T auto high) noexcept {
+			std::default_random_engine engine{ std::random_device{}() };
+
+			//This should never happen but we will support it anyway
+			if (high < low) [[unlikely]] {
+				std::swap(low,high);
+			}
+
+			return std::uniform_real_distribution<T>{ low, high }(engine);
+		}
+	};
+}
