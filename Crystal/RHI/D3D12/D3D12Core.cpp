36fb9ac1f0fac990e62afad5bb9b6ecbc2848a60
Jacster0 <jacob.ogren2@gmail.com>
2021-08-22 23:54:05 +0200

    Reorganized RHICore to use free functions

--- Crystal/RHI/D3D12/D3D12Core.cpp
+++ Crystal/RHI/D3D12/D3D12Core.cpp
@@ -5,17 +5,112 @@
 using namespace Crystal;
 using namespace Microsoft::WRL;
 
-Microsoft::WRL::ComPtr<ID3D12Device8> RHICore::m_d3d12Device{ nullptr };
-Microsoft::WRL::ComPtr<IDXGIAdapter4> RHICore::m_dxgiAdapter{ nullptr };
-bool RHICore::m_isInitialized{ false };
-DXGI_ADAPTER_DESC3 RHICore::m_adapterDesc{ 0 };
-D3D_ROOT_SIGNATURE_VERSION RHICore::m_highestRootSignatureVersion{ };
-std::array<std::unique_ptr<DescriptorAllocator>, D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES> RHICore::m_descriptorAllocators{};
+namespace impl {
+	struct Data_t {
+		Microsoft::WRL::ComPtr<ID3D12Device8> D3d12Device;
+		Microsoft::WRL::ComPtr<IDXGIAdapter4> DxgiAdapter;
 
-RHICore::RHICore() {}
+		DXGI_ADAPTER_DESC3 AdapterDesc;
+		D3D_ROOT_SIGNATURE_VERSION HighestRootSignatureVersion;
+
+		std::array<std::unique_ptr<DescriptorAllocator>, D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES> DescriptorAllocators;
+		bool IsInitialized;
+	} data;
+	
+	void CreatePhysicalDevice() {
+		ComPtr<IDXGIFactory7> dxgiFactory7;
+		ComPtr<IDXGIAdapter>  dxgiAdapter;
+		ComPtr<IDXGIAdapter4> dxgiAdapter4;
+
+		constexpr DXGI_GPU_PREFERENCE gpuPreference = DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE;
+		uint32_t createFactoryFlags = 0;
+		bool useWarp = true;
+
+#if defined(_DEBUG)
+		createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+
+		ThrowIfFailed(CreateDXGIFactory2(createFactoryFlags, IID_PPV_ARGS(&dxgiFactory7)));
+
+		auto findBestAdapter = [&](uint32_t index) {
+			return dxgiFactory7->EnumAdapterByGpuPreference(index, gpuPreference, IID_PPV_ARGS(&dxgiAdapter));
+		};
+
+		//Try to find the highest performant display subsystem 
+		for (uint32_t i = 0; findBestAdapter(i) != DXGI_ERROR_NOT_FOUND; i++) {
+			const auto hr = D3D12CreateDevice(dxgiAdapter.Get(), D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device), nullptr);
+
+			if (SUCCEEDED(hr)) {
+				ThrowIfFailed(dxgiAdapter.As(&dxgiAdapter4));
+
+				//We found a display subsystem so we don't need to fallback on a warp.
+				useWarp = false;
+				break;
+			}
+		}
+
+		//Fallback on a software renderer if Direct3D hardware is not available 
+		if (useWarp) [[unlikley]] {
+			ThrowIfFailed(dxgiFactory7->EnumWarpAdapter(IID_PPV_ARGS(&dxgiAdapter)));
+			ThrowIfFailed(dxgiAdapter.As(&dxgiAdapter4));
+		}
+
+			if (dxgiAdapter4) [[likley]] {
+				ThrowIfFailed(dxgiAdapter4->GetDesc3(&data.AdapterDesc));
+			}
+
+		data.DxgiAdapter = dxgiAdapter4;
+	}
+
+	void CreateDevice() {
+		ThrowIfFailed(D3D12CreateDevice(data.DxgiAdapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&data.D3d12Device)));
+
+		ComPtr<ID3D12InfoQueue> infoQueue;
+
+		if (SUCCEEDED(data.D3d12Device.As(&infoQueue))) {
+			infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
+			infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
+			infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
+
+			std::array severities = { D3D12_MESSAGE_SEVERITY_INFO };
+			std::array denyIds = {
+				D3D12_MESSAGE_ID_CLEARRENDERTARGETVIEW_MISMATCHINGCLEARVALUE,
+				D3D12_MESSAGE_ID_MAP_INVALID_NULLRANGE,
+				D3D12_MESSAGE_ID_UNMAP_INVALID_NULLRANGE
+			};
+
+			D3D12_INFO_QUEUE_FILTER newFilter = {
+				.DenyList = {
+					.NumSeverities = severities.size(),
+					.pSeverityList = severities.data(),
+					.NumIDs = denyIds.size(),
+					.pIDList = denyIds.data()
+				}
+			};
+
+			ThrowIfFailed(infoQueue->PushStorageFilter(&newFilter));
+		}
+	}
+
+	void EnableDebugLayer() {
+		ComPtr<ID3D12Debug> debugInterface;
+		ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface)));
+		debugInterface->EnableDebugLayer();
+	}
+
+	void ReportLiveObjects() noexcept {
+		IDXGIDebug1* dxgiDebug;
+		DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug));
+
+		dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_IGNORE_INTERNAL);
+		dxgiDebug->Release();
+	}
+}
+
+using namespace impl;
 
 void RHICore::Intialize() {
-	if (m_isInitialized) {
+	if (data.IsInitialized) {
 		return;
 	}
 
@@ -28,118 +123,36 @@ void RHICore::Intialize() {
 	CreateDevice();
 
 	for (uint32_t i = 0; i < D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES; i++) {
-		m_descriptorAllocators[i] = std::make_unique<DescriptorAllocator>(static_cast<D3D12_DESCRIPTOR_HEAP_TYPE>(i));
+		data.DescriptorAllocators[i] = std::make_unique<DescriptorAllocator>(static_cast<D3D12_DESCRIPTOR_HEAP_TYPE>(i));
 	}
 
 	D3D12_FEATURE_DATA_ROOT_SIGNATURE featureData{ .HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_1 };
 
-	if (FAILED(m_d3d12Device->CheckFeatureSupport(
+	if (FAILED(data.D3d12Device->CheckFeatureSupport(
 		D3D12_FEATURE_ROOT_SIGNATURE, 
 		&featureData, 
 		sizeof(D3D12_FEATURE_DATA_ROOT_SIGNATURE)))) 
 	{
 		featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_0;
 	}
-	m_highestRootSignatureVersion = featureData.HighestVersion;
-
-	m_isInitialized = true;
+	data.HighestRootSignatureVersion = featureData.HighestVersion;
+	data.IsInitialized               = true;
 }
 
-void RHICore::EnableDebugLayer() {
-	ComPtr<ID3D12Debug> debugInterface;
-	ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface)));
-	debugInterface->EnableDebugLayer();
+ID3D12Device8& RHICore::GetDevice() noexcept { return *data.D3d12Device.Get(); }
+
+IDXGIAdapter4& RHICore::GetPhysicalDevice() noexcept { return *data.DxgiAdapter.Get(); }
+
+const std::wstring& RHICore::GetPhysicalDeviceDescription() noexcept { return data.AdapterDesc.Description; }
+
+const D3D_ROOT_SIGNATURE_VERSION RHICore::GetHighestRootSignatureVersion() noexcept { return data.HighestRootSignatureVersion; }
+
+DescriptorAllocation RHICore::AllocateDescriptors(D3D12_DESCRIPTOR_HEAP_TYPE type, uint32_t numDescriptors) noexcept {
+	return data.DescriptorAllocators[type]->Allocate(numDescriptors);
 }
 
-DescriptorAllocation Crystal::RHICore::AllocateDescriptors(D3D12_DESCRIPTOR_HEAP_TYPE type, uint32_t numDescriptors) noexcept {
-	return m_descriptorAllocators[type]->Allocate(numDescriptors);
-}
-
-void Crystal::RHICore::ReleaseStaleDescriptors() noexcept {
+void RHICore::ReleaseStaleDescriptors() noexcept {
 	for (uint32_t i = 0; i < D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES; i++) {
-		m_descriptorAllocators[i]->ReleaseStaleDescriptors();
-	}
-}
-
-void RHICore::ReportLiveObjects() noexcept {
-	IDXGIDebug1* dxgiDebug;
-	DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug));
-
-	dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_IGNORE_INTERNAL);
-	dxgiDebug->Release();
-}
-
-void RHICore::CreatePhysicalDevice() {
-	ComPtr<IDXGIFactory7> dxgiFactory7;
-	ComPtr<IDXGIAdapter>  dxgiAdapter;
-	ComPtr<IDXGIAdapter4> dxgiAdapter4;
-
-	constexpr DXGI_GPU_PREFERENCE gpuPreference = DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE;
-	uint32_t createFactoryFlags                 = 0;
-	bool useWarp                                = true;
-
-#if defined(_DEBUG)
-	createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
-#endif
-
-	ThrowIfFailed(CreateDXGIFactory2(createFactoryFlags, IID_PPV_ARGS(&dxgiFactory7)));
-
-	auto findBestAdapter = [&](uint32_t index) {
-		return dxgiFactory7->EnumAdapterByGpuPreference(index, gpuPreference, IID_PPV_ARGS(&dxgiAdapter));
-	};
-
-	//Try to find the highest performant display subsystem 
-	for (uint32_t i = 0; findBestAdapter(i) != DXGI_ERROR_NOT_FOUND; i++) {
-		const auto hr = D3D12CreateDevice(dxgiAdapter.Get(), D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device), nullptr);
-
-		if (SUCCEEDED(hr)) {
-			ThrowIfFailed(dxgiAdapter.As(&dxgiAdapter4));
-
-			//We found a display subsystem so we don't need to fallback on a warp.
-			useWarp = false;
-			break;
-		}
-	}
-
-	//Fallback on a software renderer if Direct3D hardware is not available 
-	if (useWarp) [[unlikley]] {
-		ThrowIfFailed(dxgiFactory7->EnumWarpAdapter(IID_PPV_ARGS(&dxgiAdapter)));
-		ThrowIfFailed(dxgiAdapter.As(&dxgiAdapter4));
-	}
-
-	if (dxgiAdapter4) [[likley]] {
-		ThrowIfFailed(dxgiAdapter4->GetDesc3(&m_adapterDesc));
-	}
-
-	m_dxgiAdapter = dxgiAdapter4;
-}
-
-void RHICore::CreateDevice() {
-	ThrowIfFailed(D3D12CreateDevice(m_dxgiAdapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&m_d3d12Device)));
-
-	ComPtr<ID3D12InfoQueue> infoQueue;
-
-	if (SUCCEEDED(m_d3d12Device.As(&infoQueue))) {
-		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
-		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
-		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
-
-		std::array severities = { D3D12_MESSAGE_SEVERITY_INFO };
-		std::array denyIds = {
-			D3D12_MESSAGE_ID_CLEARRENDERTARGETVIEW_MISMATCHINGCLEARVALUE,
-			D3D12_MESSAGE_ID_MAP_INVALID_NULLRANGE,
-			D3D12_MESSAGE_ID_UNMAP_INVALID_NULLRANGE
-		};
-
-		D3D12_INFO_QUEUE_FILTER newFilter = {
-			.DenyList = {
-				.NumSeverities = severities.size(),
-				.pSeverityList = severities.data(),
-				.NumIDs = denyIds.size(),
-				.pIDList = denyIds.data() 
-			}
-		};
-
-		ThrowIfFailed(infoQueue->PushStorageFilter(&newFilter));
+		data.DescriptorAllocators[i]->ReleaseStaleDescriptors();
 	}
 }
--- Crystal/RHI/D3D12/D3D12Core.h
+++ Crystal/RHI/D3D12/D3D12Core.h
@@ -11,32 +11,14 @@
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "dxguid.lib")
 
-namespace Crystal {
-	class RHICore {
-	public:
-		RHICore();
-		static void Intialize();
+namespace Crystal::RHICore {
+	void Intialize();
 
-		static [[nodiscard]] ID3D12Device8& GetDevice() noexcept { return *m_d3d12Device.Get(); }
-		static [[nodiscard]] IDXGIAdapter4& GetPhysicalDevice() noexcept { return *m_dxgiAdapter.Get(); }
-		static [[nodiscard]] std::wstring GetPhysicalDeviceDescription() noexcept { return m_adapterDesc.Description; }
-		static [[nodiscard]] D3D_ROOT_SIGNATURE_VERSION GetHighestRootSignatureVersion() noexcept { return m_highestRootSignatureVersion; }
+	[[nodiscard]] inline ID3D12Device8& GetDevice() noexcept;
+	[[nodiscard]] inline IDXGIAdapter4& GetPhysicalDevice() noexcept;
+	[[nodiscard]] const inline std::wstring& GetPhysicalDeviceDescription() noexcept;
+	[[nodiscard]] const inline D3D_ROOT_SIGNATURE_VERSION GetHighestRootSignatureVersion() noexcept;
 
-		static [[nodiscard]] DescriptorAllocation AllocateDescriptors(D3D12_DESCRIPTOR_HEAP_TYPE type, uint32_t numDescriptors = 1) noexcept;
-		static void ReleaseStaleDescriptors() noexcept;
-	private:
-		static void CreatePhysicalDevice();
-		static void CreateDevice();
-		static void EnableDebugLayer();
-		static void ReportLiveObjects() noexcept;
-
-		static Microsoft::WRL::ComPtr<ID3D12Device8> m_d3d12Device;
-		static Microsoft::WRL::ComPtr<IDXGIAdapter4> m_dxgiAdapter;
-
-		static DXGI_ADAPTER_DESC3 m_adapterDesc;
-		static D3D_ROOT_SIGNATURE_VERSION m_highestRootSignatureVersion;
-
-		static std::array<std::unique_ptr<DescriptorAllocator>, D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES> m_descriptorAllocators;
-		static bool m_isInitialized;
-	};
+	[[nodiscard]] inline DescriptorAllocation AllocateDescriptors(D3D12_DESCRIPTOR_HEAP_TYPE type, uint32_t numDescriptors = 1) noexcept;
+	inline void ReleaseStaleDescriptors() noexcept;
 }
