af03f55064142accfb7700dff32b90f104ff9824
Jacster0 <jacob.ogren2@gmail.com>
2021-08-30 23:06:38 +0200

    Removed inline and constness

--- Crystal/RHI/D3D12/D3D12Core.h
+++ Crystal/RHI/D3D12/D3D12Core.h
@@ -14,10 +14,10 @@
 namespace Crystal::RHICore {
 	void Intialize();
 
-	[[nodiscard]] inline ID3D12Device8& GetDevice() noexcept;
-	[[nodiscard]] inline IDXGIAdapter4& GetPhysicalDevice() noexcept;
-	[[nodiscard]] const inline std::wstring& GetPhysicalDeviceDescription() noexcept;
-	[[nodiscard]] const inline D3D_ROOT_SIGNATURE_VERSION GetHighestRootSignatureVersion() noexcept;
+	[[nodiscard]] ID3D12Device8& GetDevice() noexcept;
+	[[nodiscard]] IDXGIAdapter4& GetPhysicalDevice() noexcept;
+	[[nodiscard]] const std::wstring& GetPhysicalDeviceDescription() noexcept;
+	[[nodiscard]] D3D_ROOT_SIGNATURE_VERSION GetHighestRootSignatureVersion() noexcept;
 
 	[[nodiscard]] inline DescriptorAllocation AllocateDescriptors(D3D12_DESCRIPTOR_HEAP_TYPE type, uint32_t numDescriptors = 1) noexcept;
 	inline void ReleaseStaleDescriptors() noexcept;
