2c567e36e484e12768bd4f2baf09053277d3fa6f
Jacster0 <jacob.ogren2@gmail.com>
2021-09-14 19:54:10 +0200

    Updated CommandQueue class

---
+++ Crystal/RHI/D3D12/D3D12CommandQueue.cpp
@@ -0,0 +1,83 @@
+#include "D3D12CommandQueue.h"
+#include "D3D12Core.h"
+#include "D3D12CommandContext.h"
+#include "Utils/D3D12Exception.h"
+
+#include <limits>
+#include <array>
+#include <algorithm>
+
+using namespace Crystal;
+
+CommandQueue::CommandQueue(CommandListType_t cmdListType) {
+	CommandListType type{ .Type = cmdListType };
+
+	auto& device = RHICore::GetDevice();
+
+	D3D12_COMMAND_QUEUE_DESC desc{
+		.Type     = type.As<D3D12_COMMAND_LIST_TYPE>(),
+		.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL,
+		.Flags    = D3D12_COMMAND_QUEUE_FLAG_NONE,
+		.NodeMask = 0
+	};
+
+	ThrowIfFailed(device.CreateCommandQueue(&desc, IID_PPV_ARGS(&m_d3d12CommandQueue)));
+	ThrowIfFailed(device.CreateFence(m_fenceValue, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence)));
+
+	if (type.Type == CommandListType_t::copy) {
+		m_d3d12CommandQueue->SetName(L"Copy Command Queue");
+		m_context = std::make_unique<CommandContext>(type);
+	}
+	else if (type.Type == CommandListType_t::compute) {
+		m_d3d12CommandQueue->SetName(L"Compute Command Queue");
+		m_context = std::make_unique<ComputeContext>(type);
+	}
+	else if (type.Type == CommandListType_t::direct) {
+		m_d3d12CommandQueue->SetName(L"Direct Command Queue");
+		m_context = std::make_unique<GraphicsContext>(type);
+	}
+}
+
+uint64_t CommandQueue::Submit(const CommandContext const* context) {
+	return Submit(std::array{ context });
+}
+
+uint64_t CommandQueue::Submit(std::span<const CommandContext* const> contexts) {
+	std::vector<ID3D12CommandList*> commandLists(contexts.size());
+
+	std::for_each(contexts.begin(), contexts.end(), 
+		[&commandLists](const CommandContext* const context) { 
+			commandLists.emplace_back(context->GetNativeCommandList().Get()); 
+		}
+	);
+
+	m_d3d12CommandQueue->ExecuteCommandLists(commandLists.size(), commandLists.data());
+
+	return Signal();
+}
+
+uint64_t CommandQueue::Signal() {
+	const auto fenceValue = ++m_fenceValue;
+	ThrowIfFailed(m_d3d12CommandQueue->Signal(m_fence.Get(), fenceValue));
+
+	return fenceValue;
+}
+
+void CommandQueue::WaitForFenceValue(uint64_t fenceValue) {
+	if (!IsFenceComplete(fenceValue)) {
+		if (auto event = CreateEvent(nullptr, false, false, nullptr)) {
+			ThrowIfFailed(m_fence->SetEventOnCompletion(fenceValue, event));
+
+			WaitForSingleObject(event, std::numeric_limits<DWORD>::max());
+			CloseHandle(event);
+		}
+	}
+}
+
+bool CommandQueue::IsFenceComplete(uint64_t fenceValue) const noexcept {
+	return m_fence->GetCompletedValue() >= fenceValue;
+}
+
+void CommandQueue::Wait(const CommandQueue& rhs) const {
+	ThrowIfFailed(m_d3d12CommandQueue->Wait(rhs.m_fence.Get(), rhs.m_fenceValue));
+}
--- Crystal/RHI/D3D12/D3D12CommandQueue.h
+++ Crystal/RHI/D3D12/D3D12CommandQueue.h
@@ -1,68 +1,32 @@
 #pragma once
+
 #include "../Graphics/Types/Types.h"
+
 #include <d3d12.h>
 #include <wrl.h>
 #include <atomic>
 #include <memory>
-#include <type_traits>
-#include <array>
-#include <limits>
-
-#include "D3D12CommandContext.h"
-#include "Utils/ResourceStateTracker.h"
-#include "../Core/Lib/type_traits.h"
+#include <span>
 
 namespace Crystal {
-	template<CommandListType_t CommandType = CommandListType_t::direct>
+	class CommandContext;
 	class CommandQueue {
 	public:
-		CommandQueue() {
-			if constexpr (CommandType == CommandListType_t::copy) {
-				m_context = std::make_unique<CommandContext>({ CommandType });
-			}
-			else if constexpr (CommandType == CommandListType_t::compute) {
-				m_context = std::make_unique<ComputeContext>({ CommandType });
-			}
-			else if constexpr (CommandType == CommandListType_t::direct) {
-				m_context = std::make_unique<GraphicsContext>({ CommandType });
-			}
-		}
+		explicit CommandQueue(CommandListType_t cmdListType);
 
 		//Submits commands to be executed on the GPU
-		uint64_t Submit(const CommandContext* const context) {
-			std::array<ID3D12CommandList*, 1> commandList = { context->GetNativeCommandList().Get() };
+		uint64_t Submit(const CommandContext const* context);
+		uint64_t Submit(std::span<const CommandContext* const> contexts);
 
-			m_d3d12CommandQueue->ExecuteCommandLists(commandList.size(), commandList.data());
+		[[nodiscard]] uint64_t Signal();
+		void WaitForFenceValue(uint64_t fenceValue);
 
-			uint64_t fenceValue = Signal();
-
-			return fenceValue;
-		}
-
-		[[nodiscard]] uint64_t Signal() noexcept {
-			const auto fenceValue = ++m_fenceValue;
-			m_d3d12CommandQueue->Signal(m_fence.Get(), fenceValue);
-
-			return fenceValue;
-		}
-		void WaitForFenceValue(uint64_t fenceValue) noexcept {
-			if (!IsFenceComplete(fenceValue)) {
-				auto event = CreateEvent(nullptr, false, false, nullptr);
-
-				m_fence->SetEventOnCompletion(fenceValue, event);
-
-				WaitForSingleObject(event, std::numeric_limits<DWORD>::max());
-				CloseHandle(event);
-			}
-		}
-
-		[[nodiscard]] bool IsFenceComplete(uint64_t fenceValue) noexcept { return m_fence->GetCompletedValue() >= fenceValue; }
-		void Wait(const CommandQueue& rhs) noexcept { m_d3d12CommandQueue->Wait(rhs.m_fence.Get(), rhs.m_fenceValue); }
+		[[nodiscard]] bool IsFenceComplete(uint64_t fenceValue) const noexcept;
+		void Wait(const CommandQueue& rhs) const;
 
 		[[nodiscard]] Microsoft::WRL::ComPtr<ID3D12CommandQueue> GetNativeCommandQueue() const noexcept { return m_d3d12CommandQueue; }
-		
 		[[nodiscard]] const CommandContext& GetCommandList() const noexcept { return *m_context.get(); }
-		[[nodiscard]] CommandContext& GetCommandList() noexcept { return *m_context.get(); }
+		[[nodiscard]] CommandContext& GetCommandContext() noexcept { return *m_context.get(); }
 	private:
 		Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_d3d12CommandQueue;
 		Microsoft::WRL::ComPtr<ID3D12Fence> m_fence;
