a178dc486e3cd89b7b985f5bba82358e8c83c331
Jacster0 <jacob.ogren2@gmail.com>
2021-09-16 01:44:25 +0200

    Added functions to represent formatted strings of date and time

---
+++ Crystal/Core/Time/Time.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <chrono>
+
+namespace Crystal {
+	using namespace std::chrono_literals;
+
+	[[nodiscard]] inline std::string GetCurrrentDate() noexcept {
+		const auto local = std::chrono::zoned_time{ std::chrono::current_zone(), std::chrono::system_clock::now() };
+
+		return std::format("{:%x}", local);
+	}
+
+	[[nodiscard]] inline std::string GetCurrrentTime() noexcept {
+		const auto local   = std::chrono::zoned_time{ std::chrono::current_zone(), std::chrono::system_clock::now() };
+		const auto seconds = std::stoi(std::format("{:%S}", local));
+
+		return std::format("{:%R}:{}", local, seconds);
+	}
+
+	[[nodiscard]] inline std::string GetCurrrentDateTime() noexcept {
+		const auto local   = std::chrono::zoned_time{ std::chrono::current_zone(), std::chrono::system_clock::now() };
+		const auto seconds = std::stoi(std::format("{:%S}", local));
+
+		return std::format("{:%x  %R}:{}", local, seconds);
+	}
+}
