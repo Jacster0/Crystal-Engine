30f2483cdce04b90a8856b6ffda7463414bc9a2c
Jacster0 <jacob.ogren2@gmail.com>
2021-09-16 18:58:46 +0200

    Redesigned the Time header and we now use of the ISO 8601 format instead of zone locale.

--- Crystal/Core/Time/Time.h
+++ Crystal/Core/Time/Time.h
@@ -1,24 +1,25 @@
 #pragma once
 #include <chrono>
+#include <string>
+#include "../Lib/FixedString.h"
 
 namespace Crystal {
-	[[nodiscard]] inline std::string GetCurrrentDate() noexcept {
-		const auto local = std::chrono::zoned_time{ std::chrono::current_zone(), std::chrono::system_clock::now() };
+	namespace detail {
+		template<crylib::FixedString str>
+		inline constexpr std::string date_time_format_helper() noexcept {
+			constexpr std::string_view fmt(str);
+			const std::chrono::zoned_time local = { std::chrono::current_zone(), std::chrono::system_clock::now() };
 
-		return std::format("{:%x}", local);
+			if constexpr (fmt.find("%R") != std::string::npos) {
+				return std::format(std::format("{}{}", fmt, ":{}"), local, std::stoi(std::format("{:%S}", local)));
+			}
+			else {
+				return std::format(fmt, local);
+			}
+		}
 	}
 
-	[[nodiscard]] inline std::string GetCurrrentTime() noexcept {
-		const auto local   = std::chrono::zoned_time{ std::chrono::current_zone(), std::chrono::system_clock::now() };
-		const auto seconds = std::stoi(std::format("{:%S}", local));
-
-		return std::format("{:%R}:{}", local, seconds);
-	}
-
-	[[nodiscard]] inline std::string GetCurrrentDateTime() noexcept {
-		const auto local   = std::chrono::zoned_time{ std::chrono::current_zone(), std::chrono::system_clock::now() };
-		const auto seconds = std::stoi(std::format("{:%S}", local));
-
-		return std::format("{:%x  %R}:{}", local, seconds);
-	}
+	[[nodiscard]] inline std::string current_date()      noexcept { return detail::date_time_format_helper<"{:%F}">(); }
+	[[nodiscard]] inline std::string current_time()      noexcept { return detail::date_time_format_helper<"{:%R}">(); }
+	[[nodiscard]] inline std::string current_date_time() noexcept { return detail::date_time_format_helper<"{:%F %R}">(); }
 }
