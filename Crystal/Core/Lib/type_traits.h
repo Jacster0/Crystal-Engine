2317f9d874662e2b7ac024724d84cf586e6ae9ea
Jacster0 <jacob.ogren2@gmail.com>
2021-10-26 22:03:43 +0200

    use variable templates

--- Crystal/Core/Lib/type_traits.h
+++ Crystal/Core/Lib/type_traits.h
@@ -2,14 +2,11 @@
 #include <type_traits>
 
 namespace Crystal::crylib {
-	template<class T, class U, class = void>
-	struct has_type : std::false_type {};
+	template<class, class, class = void>
+	constexpr bool has_type_v = false;
 
 	template<class T, class U>
-	struct has_type<T, U, std::void_t<typename T::U>> : std::true_type {};
-
-	template<class T, class U>
-	inline constexpr bool has_type_v = HasType<T, U>::value;
+	constexpr bool has_type_v<T, U, std::void_t<typename T::U>> = true;
 
 	template<class, template<class...> class>
 	constexpr bool is_specialization_v = false;
