b757ee329a8ec918170634326f3ad6d6b84dd721
Jacster0 <jacob.ogren2@gmail.com>
2021-11-28 22:22:41 +0100

    Update StringUtils

--- Crystal/Core/Utils/StringUtils.h
+++ Crystal/Core/Utils/StringUtils.h
@@ -5,26 +5,68 @@
 
 namespace Crystal {
 
-	[[nodiscard]] const inline std::string ToNarrow(std::wstring_view wide) {
-		const int inLength  = static_cast<int>(wide.length());
-		const int outLength = WideCharToMultiByte(CP_UTF8, 0, wide.data(), inLength, nullptr, 0, nullptr, nullptr);
+	namespace detail {
+		template<class T>
+		constexpr bool is_string_v = false;
 
-		std::string narrow(outLength, '\0');
-		WideCharToMultiByte(CP_UTF8, 0, wide.data(), inLength, narrow.data(), outLength, nullptr, nullptr);
+		template<class Elem, class Traits, class Alloc>
+		constexpr bool is_string_v<std::basic_string<Elem, Traits, Alloc>> = true;
 
-		return narrow;
+		template<class T>
+		constexpr bool can_construct_string_v = std::is_constructible_v<std::string, T>;
+
+		template<class T>
+		constexpr bool can_construct_wstring_v = std::is_constructible_v<std::wstring, T>;
+
+		std::string inline ToNarrow(std::wstring_view wide) noexcept {
+			const int inLength  = static_cast<int>(wide.length());
+			const int outLength = WideCharToMultiByte(CP_UTF8, 0, wide.data(), inLength, nullptr, 0, nullptr, nullptr);
+
+			std::string narrow(outLength, '\0');
+			WideCharToMultiByte(CP_UTF8, 0, wide.data(), inLength, narrow.data(), outLength, nullptr, nullptr);
+
+			return narrow; 
+		}
+
+		std::wstring inline ToWide(std::string_view narrow) noexcept {
+			const int inLength  = static_cast<int>(narrow.length());
+			const int outLength = MultiByteToWideChar(CP_UTF8, 0, narrow.data(), inLength, nullptr, 0);
+
+			std::wstring wide(outLength, L'\0');
+			MultiByteToWideChar(CP_UTF8, 0, narrow.data(), inLength, wide.data(), outLength);
+
+			return wide;
+		}
+	}
+
+	namespace StringConverter {
+		template<class To, class From>
+		constexpr auto ConvertTo(From&& from) noexcept -> 
+			std::enable_if_t<
+				detail::is_string_v<From> && detail::is_string_v<To>, 
+				To
+			>  
+		{
+			if constexpr (std::is_same_v<std::string, std::decay_t<To>>) {
+				if constexpr (detail::can_construct_string_v<From>) {
+					return std::string(std::forward<From>(from));
+				}
+				else {
+					return detail::ToNarrow(std::forward<From>(from));
+				}
+			}
+
+			else if constexpr (std::is_same_v<std::wstring, std::decay_t<To>>) {
+				if constexpr (detail::can_construct_wstring_v<From>) {
+					return std::wstring(std::forward<From>(from));
+				}
+				else {
+					return detail::ToWide(std::forward<From>(from));
+				}
+			}
+		}
 	}
 	
-	[[nodiscard]] const inline std::wstring ToWide(std::string_view narrow) {
-		const int inLength  = static_cast<int>(narrow.length());
-		const int outLength = MultiByteToWideChar(CP_UTF8, 0, narrow.data(), inLength, nullptr, 0);
-
-		std::wstring wide(outLength, L'\0');
-		MultiByteToWideChar(CP_UTF8, 0, narrow.data(), inLength, wide.data(), outLength);
-
-		return wide;
-	}
-
 	[[nodiscard]] constexpr inline bool IsEmptyOrWhiteSpace(std::string_view str) noexcept {
 		//Check if the string is empty
 		if (str.empty()) {
