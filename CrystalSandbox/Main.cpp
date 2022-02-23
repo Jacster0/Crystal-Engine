a917fad5c22e3aa24b50b2966d39958371ed7d99
Jacster0 <jacob.ogren2@gmail.com>
2022-02-23 19:56:45 +0100

    add sandbox for testing

---
+++ CrystalSandbox/Main.cpp
@@ -0,0 +1,41 @@
+#include "Platform/Windows/CrystalWindow.h"
+#include "Core/Application.h"
+#include "Platform/Windows/Types.h"
+#include "Core/Exceptions/CrystalException.h"
+#include "Platform/Windows/MessageBox.h"
+
+using namespace Crystal;
+int main() {
+    try {
+        Application{ ApplicationCreateInfo{} }.Run();
+        return 0;
+    }
+    catch(const CrystalException e) {
+        MessageBox::Show(
+                e.what(),
+                e.GetType(),
+                MessageBox::Buttons::OK,
+                MessageBox::Icon::Exclamation);
+
+        return 1;
+    }
+    catch(const std::exception& e){
+        MessageBox::Show(
+                e.what(),
+                "Standard exception",
+                MessageBox::Buttons::OK,
+                MessageBox::Icon::Exclamation);
+
+        return 1;
+    }
+
+    catch(...){
+        MessageBox::Show(
+                "No details available",
+                "Unknown exception",
+                MessageBox::Buttons::OK,
+                MessageBox::Icon::Exclamation);
+
+        return 1;
+    }
+}
