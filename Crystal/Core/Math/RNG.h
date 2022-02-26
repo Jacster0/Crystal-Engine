1f152e9d44e439386c521e38b9fd19f8d5241003
Jacster0 <jacob.ogren2@gmail.com>
2022-02-26 23:34:18 +0100

    ????? indenting problems clion fucks visual studio and github

--- Crystal/Core/Math/RNG.h
+++ Crystal/Core/Math/RNG.h
@@ -4,8 +4,8 @@
 #include <algorithm>
 
 namespace Crystal::Math {
-	class Rng {
-	public:
+    class Rng {
+    public:
         template<typename T>
         requires std::integral<T>
         [[nodiscard]] static constexpr auto Random(T low, T high) noexcept {
@@ -17,19 +17,19 @@ namespace Crystal::Math {
             }
 
             return std::uniform_int_distribution<T>{ low, high }(engine);
-		}
+        }
 
-		template<typename T>
-		requires std::floating_point<T>
+        template<typename T>
+        requires std::floating_point<T>
         [[nodiscard]] static constexpr auto Random(T low, T high) noexcept {
             std::default_random_engine engine{ std::random_device{}() };
 
             //This should never happen but we will support it anyway
             if (high < low) [[unlikely]] {
-				std::swap(low,high);
-			}
+                std::swap(low,high);
+            }
 
             return std::uniform_real_distribution<T>{ low, high }(engine);
-		}
-	};
+        }
+    };
 }
