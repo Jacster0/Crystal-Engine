9a713ae9c4d1eb2a27bc4fb596a1335f3d71389d
Jacster0 <jacob.ogren2@gmail.com>
2022-02-26 23:32:17 +0100

    fix formatting

--- Crystal/Core/Math/RNG.h
+++ Crystal/Core/Math/RNG.h
@@ -6,30 +6,30 @@
 namespace Crystal::Math {
 	class Rng {
 	public:
-		template<typename T>
+        template<typename T>
         requires std::integral<T>
-		[[nodiscard]] static constexpr auto Random(T low, T high) noexcept {
-			std::default_random_engine engine{ std::random_device{}() };
+        [[nodiscard]] static constexpr auto Random(T low, T high) noexcept {
+            std::default_random_engine engine{ std::random_device{}() };
 
-			//This should never happen but we will support it anyway
-			if (high < low) [[unlikely]] {
-				std::swap(low,high);
-			}
+            //This should never happen but we will support it anyway
+            if (high < low) [[unlikely]] {
+                std::swap(low,high);
+            }
 
-			return std::uniform_int_distribution<T>{ low, high }(engine);
+            return std::uniform_int_distribution<T>{ low, high }(engine);
 		}
 
 		template<typename T>
 		requires std::floating_point<T>
-		[[nodiscard]] static constexpr auto Random(T low, T high) noexcept {
-			std::default_random_engine engine{ std::random_device{}() };
+        [[nodiscard]] static constexpr auto Random(T low, T high) noexcept {
+            std::default_random_engine engine{ std::random_device{}() };
 
-			//This should never happen but we will support it anyway
-			if (high < low) [[unlikely]] {
+            //This should never happen but we will support it anyway
+            if (high < low) [[unlikely]] {
 				std::swap(low,high);
 			}
 
-			return std::uniform_real_distribution<T>{ low, high }(engine);
+            return std::uniform_real_distribution<T>{ low, high }(engine);
 		}
 	};
 }
